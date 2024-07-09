0519f4dd95eaa0a2a06a1c0949ccbf3a3fefce56
Vladyslav Bychkovskyi <bickovskijvlad53@gmail.com>
2024-07-10 00:07:57 +0200

    test5

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://nishiyuuu.github.io/Golden-Grill/",
+    site: "https://nishiyuuu.github.io/",
     base: "/Golden-Grill/",
 });
